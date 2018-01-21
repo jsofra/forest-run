cea67d85a5dd220e60c735e7ee0d7b900539352a
James Sofra <james.sofra@gmail.com>
2018-01-21 22:17:16 +1100

    Do state updates via a async channel.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [impi "0.0.11-SNAPSHOT"]]
+                 [impi "0.0.11-SNAPSHOT"]
+                 [org.clojure/core.async "0.4.474"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.14"]]
   :cljsbuild
--- src/cljs/forest_run/ui.cljs
+++ src/cljs/forest_run/ui.cljs
@@ -1,7 +1,8 @@
 (ns forest-run.ui
   (:require [forest-run.impi]
             [impi.core :as impi]
-            [forest-run.core :as core]))
+            [forest-run.core :as core]
+            [cljs.core.async :as async]))
 
 (enable-console-print!)
 
@@ -37,24 +38,26 @@
              (render-card card
                           (card-pos c-idx r-idx)
                           [c-idx r-idx]
-                          (<= r-idx (inc r)))))
+                          (<= r-idx (+ r 3)))))
          flatten
          (map (fn [card] [(:game/index card) card]))
          (into {position (-> (render-card core/player-card
                                           (card-pos c r)
                                           position
                                           true)
-                             (assoc :pixi.event/pointer-down [:player-down position])
-                             (assoc :pixi.event/pointer-up   [:player-up position])
-                             (assoc :pixi.event/pointer-up-outside [:player-up position])
-                             (dissoc :pixi.event/click))}))))
+                             (assoc :pixi.event/pointer-down [:player-down])
+                             (assoc :pixi.event/pointer-up   [:player-up])
+                             (assoc :pixi.event/pointer-up-outside [:player-up])
+                             (dissoc :pixi.event/click))})
+         (into (sorted-map-by >)))))
 
-(defonce gui-state (atom nil))
-(defonce game-state (atom (core/init-game-state)))
+(defonce state (atom nil))
+(defonce updates-chan (async/chan))
 
-(defn update-stage-pos!
-  [delta]
-  (swap! gui-state update-in [:pixi/stage :pixi.object/position] #(mapv + % delta)))
+
+(defn update-stage-pos
+  [state delta]
+  (update-in state [:pixi/stage :pixi.object/position] #(mapv + % delta)))
 
 (defn update-card
   [state id prop update-fn]
@@ -66,10 +69,6 @@
               id
               prop]
              update-fn))
-
-(defn update-card!
-  [idx prop update-fn]
-  (swap! gui-state update-card idx prop update-fn))
 
 (defn update-card-tints
   [update-fn state idxs tint]
@@ -89,7 +88,7 @@
 
 (defn update-move-tints
   [state tint-fn]
-  (let [{:moves/keys [valid invalid]} (core/moves @game-state)
+  (let [{:moves/keys [valid invalid]} (core/moves (:game/state state))
         valid-idxs                    (vals valid)
         invalid-idxs                  (vals invalid)]
     (-> state
@@ -101,52 +100,67 @@
 
 (defn init-stage! []
   (reset!
-   gui-state
-   {:pixi/renderer
-    {:pixi.renderer/size             [js/window.innerWidth js/window.innerHeight]
-     :pixi.renderer/background-color 0x0a1c5e
-     :pixi.renderer/transparent?     false}
-    :pixi/listeners
-    {:mouse-move
-     (fn [x]
-       (let [event (-> x .-data .-originalEvent)]
-         (when (and (not (zero? (.-buttons event)))
-                    (not (zero? (.-movementY event))))
-           (update-stage-pos! [0 (.-movementY event)]))))
-     :card-click
-     (fn [_ id] (update-card! id :card/rotation inc))
-     :player-down
-     (fn [x _]
-       (swap! gui-state update-move-tints add-card-tints))
-     :player-up
-     (fn [x _]
-       (swap! gui-state update-move-tints sub-card-tints))}
-    :pixi/stage
-    {:impi/key                 :stage
-     :pixi.object/type         :pixi.object.type/container
-     :pixi.object/position     [(stage-x) (- (* (+ card-h card-spacing) 3)
-                                             (/ card-h 2))]
-     :pixi.object/interactive? true
-     :pixi.event/mouse-move    [:mouse-move]
-     :pixi.container/children
-     {:deck
-      {:impi/key                :deck
-       :pixi.object/type        :pixi.object.type/container
-       :pixi.container/children (render-deck (last @game-state))}}}}))
+   state
+   (let [game-state (core/init-game-state)]
+     {:pixi/renderer
+      {:pixi.renderer/size             [js/window.innerWidth
+                                        js/window.innerHeight]
+       :pixi.renderer/background-color 0x0a1c5e
+       :pixi.renderer/transparent?     false}
+      :pixi/listeners
+      {:mouse-move
+       (fn [e]
+         (async/put! updates-chan
+                     (let [event (-> e .-data .-originalEvent)]
+                       (if (and (not (zero? (.-buttons event)))
+                                (not (zero? (.-movementY event))))
+                         #(update-stage-pos % [0 (.-movementY event)])
+                         identity))))
+       :card-click
+       (fn [_ id]
+         (async/put! updates-chan #(update-card % id :card/rotation inc)))
+       :player-down
+       (fn [_]
+         (async/put! updates-chan #(update-move-tints % add-card-tints)))
+       :player-up
+       (fn [_]
+         (async/put! updates-chan #(update-move-tints % sub-card-tints)))}
+      :pixi/stage
+      {:impi/key                 :stage
+       :pixi.object/type         :pixi.object.type/container
+       :pixi.object/position     [(stage-x) (- (* (+ card-h card-spacing) 3)
+                                               (/ card-h 2))]
+       :pixi.object/interactive? true
+       :pixi.event/mouse-move    [:mouse-move]
+       :pixi.container/children
+       {:deck
+        {:impi/key                :deck
+         :pixi.object/type        :pixi.object.type/container
+         :pixi.container/children (render-deck (last game-state))}}}
+      :game/state game-state})))
 
 (defn update-gui-state! [game-state]
-  (swap! gui-state
+  (swap! state
          update-in
          [:pixi/stage :deck :pixi.container/children]
          #(render-deck (last game-state))))
 
 (let [element (.getElementById js/document "app")]
-  (impi/mount :game @gui-state element)
-  (add-watch game-state ::gui (fn [_ _ _ s] (update-gui-state! s)))
-  (add-watch gui-state ::mount (fn [_ _ _ s] (impi/mount :game s element))))
+  (impi/mount :game @state element)
+  (add-watch state ::mount (fn [_ _ _ s] (impi/mount :game s element))))
+
+(defonce ticker (doto (js/PIXI.ticker.Ticker.)
+              (.stop)))
+
+(.add ticker
+      (fn [delta-time]
+        (swap! state (loop [updates identity]
+                       (if-let [update (async/poll! updates-chan)]
+                         (recur (comp updates update))
+                         updates)))))
 
 (defn resize-renderer! []
-  (swap! gui-state
+  (swap! state
          #(-> %
               (assoc-in [:pixi/renderer :pixi.renderer/size]
                         [js/window.innerWidth js/window.innerHeight])
