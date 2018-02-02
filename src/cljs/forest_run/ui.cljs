c8236bcc8058edaba000555ccc7e20dfb0a3f198
James Sofra <james.sofra@gmail.com>
2018-02-03 07:18:07 +1100

    Switch to having a state model and rendering from that.

--- src/cljs/forest_run/ui.cljs
+++ src/cljs/forest_run/ui.cljs
@@ -11,40 +11,69 @@
 (def card-size [card-w card-h])
 (def card-spacing 8)
 
-(defn render-card [{:keys [rank suit] :as card}
-                   {:keys [pos index revealed]
-                    :or   {revealed true}}]
-  (let [card-name (keyword (str (name rank) "-" (name suit)))]
-    {:impi/key                 card-name
-     :pixi.object/type         :pixi.object.type/sprite
-     :pixi.object/position     pos
-     :card/rotation            0
-     :card/revealed            revealed
-     :game/index               index
-     :pixi.sprite/anchor       [0.5 0.5]
-     :pixi.sprite/tint         0xFFFFFF
-     :pixi.object/interactive? true
-     :pixi.event/click         [:card-click index]
-     :pixi.sprite/texture
-     {:pixi.texture/source (if revealed
-                             (str "img/" (name card-name) ".png")
-                             (str "img/back.png"))}}))
-
 (defn render-attack
-  [{:keys [impi/key]} attack]
-  [{:impi/key             (str (name key) "-text")
-    :pixi.object/type     :pixi.object.type/text
-    :pixi.object/position [0 0]
-    :pixi.text/anchor     [0.5 0.5]
-    :pixi.text/text       attack
-    :pixi.text/style
-    {:pixi.text.style/align            "left"
-     :pixi.text.style/fill             0xff0000
-     :pixi.text.style/font-weight      "bold"
-     :pixi.text.style/font-family      "Arial"
-     :pixi.text.style/font-size        42
-     :pixi.text.style/stroke           0x000000
-     :pixi.text.style/stroke-thickness 6}}])
+  [card-name attack]
+  {:impi/key             (str (name card-name) "-text")
+   :pixi.object/type     :pixi.object.type/text
+   :pixi.object/position [0 0]
+   :pixi.text/anchor     [0.5 0.5]
+   :pixi.text/text       attack
+   :pixi.text/style
+   {:pixi.text.style/align            "left"
+    :pixi.text.style/fill             0xff0000
+    :pixi.text.style/font-weight      "bold"
+    :pixi.text.style/font-family      "Arial"
+    :pixi.text.style/font-size        42
+    :pixi.text.style/stroke           0x000000
+    :pixi.text.style/stroke-thickness 6}})
+
+(defn render-card
+  ([card]
+   (render-card card nil))
+  ([{:keys [rank suit pos index revealed attack]
+     :or   {revealed true}
+     :as   card}
+    player]
+   (let [card-name (keyword (str (name rank) "-" (name suit)))]
+     {:impi/key                 card-name
+      :pixi.object/type         :pixi.object.type/sprite
+      :pixi.object/position     pos
+      :card/rotation            0
+      :card/revealed            revealed
+      :game/index               index
+      :pixi.sprite/anchor       [0.5 0.5]
+      :pixi.sprite/tint         0xFFFFFF
+      :pixi.object/interactive? true
+      :pixi.event/click         [:card-click index]
+      :pixi.sprite/texture
+      {:pixi.texture/source (if revealed
+                              (str "img/" (name card-name) ".png")
+                              (str "img/back.png"))}
+      :pixi.container/children (if (and revealed (:player/selected player))
+                                 [(render-attack card-name attack)]
+                                 [])})))
+
+(defn render-player [{:keys [pos index] :as card}
+                     {:player/keys [selected]}]
+  (let [rendered-card (-> (render-card card)
+                          (assoc :pixi.event/pointer-down
+                                 [:player-down]
+                                 :pixi.event/pointer-up
+                                 [:player-up]
+                                 :pixi.event/pointer-up-outside
+                                 [:player-up])
+                          (dissoc :pixi.event/click))]
+    (if selected
+      {:pixi.object/type     :pixi.object.type/container
+       :pixi.container/children
+       [{:impi/key             :player/drop-shadow
+         :pixi.object/type     :pixi.object.type/sprite
+         :pixi.object/position pos
+         :pixi.sprite/anchor   [0.5 0.5]
+         :pixi.sprite/texture
+         {:pixi.texture/source "img/dropshadow.png"}}
+        (update rendered-card :pixi.object/position #(mapv - % [4 4]))]}
+      rendered-card)))
 
 (defn card-pos
   [c-idx r-idx]
@@ -52,174 +81,96 @@
         [(Math/abs (- c-idx 2)) (* r-idx -1)]
         card-size))
 
-(defn render-deck [{:keys [deck position]}]
-  (let [[c r]   position
-        attacks (core/apply-attacks deck)]
+(defn render-deck [{:keys [game-state player]}]
+  (let [{:keys [deck position]}       (last game-state)
+        [c r]                         position
+        attacks                       (core/apply-attacks deck)
+        {:moves/keys [valid invalid]} (core/moves game-state)]
     (->> (for [[r-idx row] (map-indexed vector deck)]
-           (for [[c-idx card] (map-indexed vector row)]
-             (render-card card
-                          {:pos      (card-pos c-idx r-idx)
-                           :index    [c-idx r-idx]
-                           :revealed (<= r-idx (+ r 3))})))
+           (for [[c-idx card] (map-indexed vector row)
+                 :let         [index  [c-idx r-idx]
+                               attack (core/lookup-card attacks index)]]
+             (cond-> (render-card (merge card
+                                         {:pos      (apply card-pos index)
+                                          :index    index
+                                          :revealed (<= r-idx (+ r 3))
+                                          :attack   attack})
+                                  player)
+               (:player/selected player)
+               (cond->
+                   (contains? (set (vals valid)) index)
+                 (assoc :pixi.sprite/tint 0xccffcc)
+                 (contains? (set (vals invalid)) index)
+                 (assoc :pixi.sprite/tint 0xffcccc)))))
          flatten
          (map (fn [card] [(:game/index card) card]))
-         (into {position (-> (render-card core/player-card
-                                          {:pos   (card-pos c r)
-                                           :index position})
-                             (assoc :pixi.event/pointer-down
-                                    [:player-down position])
-                             (assoc :pixi.event/pointer-up
-                                    [:player-up position])
-                             (assoc :pixi.event/pointer-up-outside
-                                    [:player-up position])
-                             (dissoc :pixi.event/click))})
+         (into {position (render-player (merge core/player-card
+                                               {:pos   (card-pos c r)
+                                                :index position})
+                                        player)})
          (into (sorted-map-by >)))))
-
-
-(defonce state (atom nil))
-(defonce updates-chan (async/chan))
-
-
-(defn update-stage-pos
-  [state delta]
-  (update-in state [:pixi/stage :pixi.object/position] #(mapv + % delta)))
-
-(defn update-card
-  [state id prop update-fn]
-  (update-in state
-             [:pixi/stage
-              :pixi.container/children
-              :deck
-              :pixi.container/children
-              id
-              prop]
-             update-fn))
-
-(defn update-card-tints
-  [update-fn state idxs tint]
-  (reduce (fn [accum-state idx]
-            (update-card accum-state
-                         idx
-                         :pixi.sprite/tint
-                         #(update-fn % tint)))
-          state
-          idxs))
-
-(def add-card-tints (partial update-card-tints +))
-(def sub-card-tints (partial update-card-tints -))
-
-(def valid-move-tint (- 0xccffcc 0xFFFFFF))
-(def invalid-move-tint (- 0xffcccc 0xFFFFFF))
-
-(defn update-move-tints
-  [state tint-fn]
-  (let [{:moves/keys [valid invalid]} (core/moves (:game/state state))
-        valid-idxs                    (vals valid)
-        invalid-idxs                  (vals invalid)]
-    (-> state
-        (tint-fn valid-idxs valid-move-tint)
-        (tint-fn invalid-idxs invalid-move-tint))))
-
-(defn update-card-attacks
-  [state show-attacks?]
-  (let [attacks (-> state :game/state last :deck core/apply-attacks)]
-    (update-in state
-               [:pixi/stage
-                :pixi.container/children
-                :deck
-                :pixi.container/children]
-               (partial reduce-kv
-                        (fn [m k v]
-                          (let [attack (core/lookup-card attacks (:game/index v))]
-                            (assoc m k (if (:card/revealed v)
-                                         (assoc v
-                                                :pixi.container/children
-                                                (if show-attacks?
-                                                  (render-attack v attack)
-                                                  []))
-                                         v))))
-                        {}))))
-
-(defn update-player-card
-  [state idx up?]
-  (let [pos-offset [4 4]]
-    (cond-> state
-      up? (-> (assoc-in [:pixi/stage :pixi.container/children :drop-shadow]
-                        {:impi/key             :player/drop-shadow
-                         :pixi.object/type     :pixi.object.type/sprite
-                         :pixi.object/position (apply card-pos idx)
-                         :pixi.sprite/anchor   [0.5 0.5]
-                         :pixi.sprite/texture
-                         {:pixi.texture/source "img/dropshadow.png"}})
-              (update-in [:pixi/stage :pixi.container/children]
-                         #(into (sorted-map-by >) %))
-              (update-card idx :pixi.object/position
-                           #(mapv - % pos-offset)))
-      (not up?) (-> (update-in [:pixi/stage :pixi.container/children]
-                               dissoc
-                               :drop-shadow)
-                    (update-card idx :pixi.object/position
-                                 #(mapv + % pos-offset))))))
 
 (def stage-x #(- (* js/window.innerWidth 0.5)
                  (+ card-w card-spacing)))
 
+(defonce state (atom nil))
+(defonce updates-chan (async/chan))
+
+(defn update-stage-y
+  [state y-delta]
+  (update-in state [:stage :stage/y] #(+ % y-delta)))
+
+(defn update-player-selection
+  [state selected?]
+  (assoc-in state [:player :player/selected] selected?))
+
+(defn render-state
+  [{:keys [canvas stage game-state] :as state}]
+  {:pixi/renderer
+   (let [{:canvas/keys [color]} canvas]
+     {:pixi.renderer/size             [js/window.innerWidth js/window.innerHeight]
+      :pixi.renderer/background-color color
+      :pixi.renderer/transparent?     false})
+   :pixi/listeners
+   {:mouse-move
+    (fn [e]
+      (async/put! updates-chan
+                  (let [event (-> e .-data .-originalEvent)]
+                    (if (and (not (zero? (.-buttons event)))
+                             (not (zero? (.-movementY event))))
+                      #(update-stage-y % (.-movementY event))
+                      identity))))
+    :player-down
+    (fn [_]
+      (async/put! updates-chan #(update-player-selection % true)))
+    :player-up
+    (fn [_]
+      (async/put! updates-chan #(update-player-selection % false)))}
+   :pixi/stage
+   {:impi/key                 :stage
+    :pixi.object/type         :pixi.object.type/container
+    :pixi.object/position     [(stage-x) (:stage/y stage)]
+    :pixi.object/interactive? true
+    :pixi.event/mouse-move    [:mouse-move]
+    :pixi.container/children
+    {:deck
+     {:impi/key                :deck
+      :pixi.object/type        :pixi.object.type/container
+      :pixi.container/children (render-deck state)}}}})
+
 (defn init-stage! []
   (reset!
    state
-   (let [game-state (core/init-game-state)]
-     {:pixi/renderer
-      {:pixi.renderer/size             [js/window.innerWidth
-                                        js/window.innerHeight]
-       :pixi.renderer/background-color 0x0a1c5e
-       :pixi.renderer/transparent?     false}
-      :pixi/listeners
-      {:mouse-move
-       (fn [e]
-         (async/put! updates-chan
-                     (let [event (-> e .-data .-originalEvent)]
-                       (if (and (not (zero? (.-buttons event)))
-                                (not (zero? (.-movementY event))))
-                         #(update-stage-pos % [0 (.-movementY event)])
-                         identity))))
-       :card-click
-       (fn [_ id]
-         (async/put! updates-chan #(update-card % id :card/rotation inc)))
-       :player-down
-       (fn [_ idx]
-         (async/put! updates-chan #(-> %
-                                       (update-move-tints add-card-tints)
-                                       (update-card-attacks true)
-                                       (update-player-card idx true))))
-       :player-up
-       (fn [_ idx]
-         (async/put! updates-chan #(-> %
-                                       (update-move-tints sub-card-tints)
-                                       (update-card-attacks false)
-                                       (update-player-card idx false))))}
-      :pixi/stage
-      {:impi/key                 :stage
-       :pixi.object/type         :pixi.object.type/container
-       :pixi.object/position     [(stage-x) (- (* (+ card-h card-spacing) 3)
-                                               (/ card-h 2))]
-       :pixi.object/interactive? true
-       :pixi.event/mouse-move    [:mouse-move]
-       :pixi.container/children
-       {:deck
-        {:impi/key                :deck
-         :pixi.object/type        :pixi.object.type/container
-         :pixi.container/children (render-deck (last game-state))}}}
-      :game/state game-state})))
-
-(defn update-gui-state! []
-  (swap! state
-         assoc-in
-         [:pixi/stage :pixi.container/children :deck :pixi.container/children]
-         (render-deck (last (:game/state @state)))))
+   {:game-state (core/init-game-state)
+    :canvas     #:canvas {:color  0x0a1c5e}
+    :stage      #:stage {:y (- (* (+ card-h card-spacing) 3)
+                               (/ card-h 2))}
+    :player     #:player {:selected false}}))
 
 (let [element (.getElementById js/document "app")]
-  (impi/mount :game @state element)
-  (add-watch state ::mount (fn [_ _ _ s] (impi/mount :game s element))))
+  (when @state (impi/mount :game (render-state @state) element))
+  (add-watch state ::mount (fn [_ _ _ s]
+                             (impi/mount :game (render-state s) element))))
 
 (defonce ticker (doto (js/PIXI.ticker.Ticker.)
               (.stop)))
@@ -230,16 +181,6 @@
                        (if-let [update (async/poll! updates-chan)]
                          (recur (comp updates update))
                          updates)))))
-
-(defn resize-renderer! []
-  (swap! state
-         #(-> %
-              (assoc-in [:pixi/renderer :pixi.renderer/size]
-                        [js/window.innerWidth js/window.innerHeight])
-              (assoc-in [:pixi/stage :pixi.object/position 0]
-                        (stage-x)))))
-
-(.addEventListener js/window "resize" resize-renderer!)
 
 (defn start []
   (do (init-stage!)
